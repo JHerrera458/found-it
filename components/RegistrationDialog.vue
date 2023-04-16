eb19e1626811cbb8fefb0879b040d7ce2e93dc06
JHerrera458 <julianherrera33@gmail.com>
2023-04-16 12:27:02 -0500

    Instalacion del JSON server

---
+++ components/RegistrationDialog.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h2>Register</h2>
+      <form>
+        <label for="newUsername">Username:</label>
+        <input type="text" id="newUsername" v-model="newUsername">
+        <br>
+        <label for="newPassword">Password:</label>
+        <input type="password" id="newPassword" v-model="newPassword">
+        <br>
+        <label for="confirmPassword">Confirm Password:</label>
+        <input type="password" id="confirmPassword" v-model="confirmPassword">
+        <br>
+        <button type="button" @click="submitRegistration">Register</button>
+        <button type="button" @click="$emit('close')">Cancel</button>
+      </form>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+
+    data() {
+      return {
+        newUsername: '',
+        newPassword: '',
+        confirmPassword: ''
+      }
+    },
+    methods: {
+      submitRegistration() {
+        // Handle registration submission here
+        // Make sure to validate the input fields before submitting
+      }
+    }
+  }
+  </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
-    <v-container>
+    <v-container grid-list-xs>
         <div style="margin-top: 60px;"></div>
 
         <h1>Hola mundo</h1>
     </v-container>
 </template>
-
+  
 <script>
-
+export default {
+    layout: 'blank',
+    data() {
+        return {
+        }
+    }
+}
 </script>
+  
